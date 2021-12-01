476ef13975d22ef174be127540689dd87bb3b17f
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-12-01 11:59:39 +0300

    [fix] icons bug

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
 const Icon = ({ name, size }) => {
+    const iconName = (name) => {
+        switch (name) {
+            case 'dust':
+            case 'sand':
+            case 'ash':
+                return 'dust';
+    
+            case 'mist':
+            case 'smoke':
+            case 'haze':
+            case 'fog':
+                return 'mist';
+    
+            default:
+                return name;
+        }
+    }
+
     return (
         <svg width={size} height={size}>
-            <use href={`#${name}`} />
+            <use href={`#${iconName(name)}`} />
         </svg>
     )
 }
